6aa868aa130e2d8ba8afc863bde06f95f694158b
ras0q <ras0q@users.noreply.github.com>
2023-06-09 17:26:28 +0900

    :sparkles: add api.proto.v2.RoomService

---
+++ internal/handler/room.go
@@ -0,0 +1,29 @@
+package handler
+
+import (
+	"context"
+
+	protov2 "github.com/21hack02win/nascalay-v2-backend/pkg/bufgen/api/proto/v2"
+	"github.com/21hack02win/nascalay-v2-backend/pkg/bufgen/api/proto/v2/protov2connect"
+	"github.com/bufbuild/connect-go"
+)
+
+type roomHandler struct{}
+
+func NewRoomHandler() protov2connect.RoomServiceHandler {
+	return &roomHandler{}
+}
+
+type roomServiceCreateRequest = connect.Request[protov2.RoomServiceCreateRequest]
+type roomServiceCreateResponse = connect.Response[protov2.RoomServiceCreateResponse]
+
+func (h *roomHandler) Create(context.Context, *roomServiceCreateRequest) (*roomServiceCreateResponse, error) {
+	panic("implement me")
+}
+
+type roomServiceJoinRequest = connect.Request[protov2.RoomServiceJoinRequest]
+type roomServiceJoinResponse = connect.Response[protov2.RoomServiceJoinResponse]
+
+func (h *roomHandler) Join(context.Context, *roomServiceJoinRequest) (*roomServiceJoinResponse, error) {
+	panic("implement me")
+}
